985b7954a410271ba71a01f3ddd698441141dcff
davidbritozkk <davidbrito200511@gmail.com>
2025-11-23 13:02:28 -0300

    first commit

---
+++ src/main/java/com/ucsal/disciplina_microsservice/DisciplinaMicrosserviceApplication.java
@@ -0,0 +1,13 @@
+package com.ucsal.disciplina_microsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisciplinaMicrosserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisciplinaMicrosserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ucsal/disciplina_microsservice/DisciplinaMicrosserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucsal.disciplina_microsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisciplinaMicrosserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
